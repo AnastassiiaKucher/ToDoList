c7b3fbc2063ce7984c74570a670c56548d8c489f
Glitch (glitch-hello-website) <none>
2021-09-08 10:43:30 +0000

    🚓🌴 Checkpoint ./index.html:32888835/4203 ./style.css:32888835/5510 ./script.js:32888835/1502

--- script.js
+++ script.js
@@ -1,27 +1,20 @@
-/*
-This is your site JavaScript code - you can add interactivity and carry out processing
-- Initially the JS writes a message to the console, and moves a button you can add from the README
-*/
+const inputField = document.querySelector ('.inputField');
+const btn = document.querySelector('.btn');
+const toDoContainer = document.querySelector('.toDoContainer');
 
-// Print a message in the browser's dev tools console each time the page loads
-// Use your menus or right-click / control-click and choose "Inspect" > "Console"
-console.log("Hello 🌎");
+btn.addEventListener('click', ()=>{
+    const item = document.createElement('li');
+    item.innerText = inputField.value;
+    item.classList.add('toDoAdded');
+    toDoContainer.appendChild(item);
+    inputField.value = '';
 
-/* 
-Make the "Click me!" button move when the visitor clicks it:
-- First add the button to the page by following the "Next steps" in the README
-*/
-const btn = document.querySelector("button"); // Get the button from the page
-// Detect clicks on the button
-if (btn) {
-  btn.onclick = function() {
-    // The JS works in conjunction with the 'dipped' code in style.css
-    btn.classList.toggle("dipped");
-  };
-}
+    item.addEventListener('click',  ()=>{
+        item.classList.add('toDoCompleted')
+    })
 
-// This is a single line JS comment
-/*
-This is a comment that can span multiple lines 
-- use comments to make your own notes!
-*/
+    item.addEventListener('dblclick',  ()=>{
+        toDoContainer.removeChild(item);
+    })
+
+})
